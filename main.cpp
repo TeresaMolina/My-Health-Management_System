b5742c62c6a82a169d442856fcc1c08e56ebfb35
Teresa Brianna Molina <teresa.molina01@utrgv.edu>
2025-02-14 21:41:32 +0000

    new commit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 //main.cpp
+
 #include "meal_plans.h"
 #include "workouts.h"
 #include "calorie_tracker.h"
-
+#include <iostream>;
 int main(){
 	while (true){
 	
-int choice
+int choice;
 		std::cout << "\nWelcome to the App Menu: \n";
 		std::cout << "\n1. Meal Plan Options\n 2. Workout Suggestions\n 3. Track My Calories\n 4. Exit\n";
 		std::cout << "Please Enter Your Choice";
-		std >> choice;
+		std::cin >> choice;
 
 	switch (choice){
 		case 1:
