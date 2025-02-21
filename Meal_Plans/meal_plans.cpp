6cf791b6d30efc4052381d2aacaf06e41372a54e
daniran0710 <daniella.rangel01@utrgv.edu>
2025-02-20 19:54:27 -0600

    Update meal_plans.cpp

--- Meal_Plans/meal_plans.cpp
+++ Meal_Plans/meal_plans.cpp
@@ -2,5 +2,25 @@
 #include "meal_plans.h"
 #include <iostream>
 void displayMealPlans() {
-    std::cout << "Displaying meal plans...\n";
+    int choice;
+    cout << "Select a Meal Plan: \n";
+    cout << "1. Weight Loss Plan\n2. Weight Gain Plan\n"
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    if (choice == 1) {
+        cout << "\nWeight Loss Meal Plan:\n";
+        cout << "Breakfast:...\n";
+        cout << "Lunch:...\n";
+        cout << "Dinner:...\n";
+        cout << "Snacks:...\n";
+    }
+
+    else (choice == 2) {
+        cout << "\nWeight Gain Meal Plan:\n";
+        cout << "Breakfast: Two Scrambled Eggs...\n";
+        cout << "Lunch: Grilled Chicken\n"; //vegetables...rice...
+        cout << "Dinner:...\n";
+        cout << "Snacks:...\n"; //peanut butter...
+    }
 }
