fc5e00d079fad541c651abbc5c5e9178f005164d
daniran0710 <daniella.rangel01@utrgv.edu>
2025-04-24 21:30:07 -0500

    Update meal_plans.h

--- Meal_Plans/meal_plans.h
+++ Meal_Plans/meal_plans.h
@@ -4,7 +4,26 @@
 #define MEAL_PLANS_H
 
 #include <string>
+#include <vector>
 
-void displayFilteredPlans(const std::string& preference);
+class MealPlans {
+public:
+  MealPlans();
+  void displayMealPlan(const string& goal) const;
+
+private:
+  vector<string> weightLossBreakfasts;
+  vector<string> weightLossLunches;
+  vector<string> weightLossDinners;
+  vector<string> weightLossSnacks;
+  
+  vector<string> weightGainBreakfasts;
+  vector<string> weightGainLunches;
+  vector<string> weightGainDinners;
+  vector<string> weightGainSnacks;
+
+void populateMealPlans();
+void displayMeals(const vector<string>& meals, const string& type) const;
+};
 
 #endif // MEAL_PLANS_H
